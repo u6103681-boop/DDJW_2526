6941dfd2d3899d70bb68acb536bb90255c443199
u6103681 <u6103681@campus.udg.edu>
2026-03-15 22:19:45 +0100

    Arreclat error js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 load', function() {
 
     $("#play").click(function(){
-    function(){
         window.location.assign("./html/game.html");
     });
-
     $("#options").click(function(){
-    function(){
         console.error("Opció no implementada");
     });
-
     $("#saves").click(function(){
-    function(){
         console.error("Opció no implementada");
     });
 
     $("#exit").click(function(){
-    function(){
         console.warn("No es pot sortir!");
     });
 });
